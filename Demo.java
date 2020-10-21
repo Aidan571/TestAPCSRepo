8411cf0f1ba2274479395dc23d1ac7aeabc6b6a1
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-10-21 17:52:26 -0400

    I tested this program and it works now

--- Demo.java
+++ Demo.java
@@ -1,5 +1,6 @@
 public class Demo{
   public static void printLoop(int n){
+
   for(int i = 1; i <= n; i++){
     for(int j = 1; j <= n - i + 1; j++ ){
        System.out.print(i);
@@ -31,13 +32,29 @@ return result + "}";
 public static int[][] create2DArray(int rows, int cols, int maxValue){
   int[][] arr = new int[rows][cols];
   for(int i = 0; i < cols; i++){
-    for(int h = 0; h < rows; h ++){
+    for(int h = 0; h < rows; h++){
+      arr[h][i] = (int)(Math.random() * ((maxValue-0)+1));
+    }
+  }
+  return arr;
+}
+public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue){
+  int myCols = (int)(Math.random() * ((cols-0)+1));
+  int[][] arr = new int[rows][myCols];
+  for(int i = 0; i < myCols; i++){
+    for(int h =  0; h < rows; h++){
       arr[h][i] = (int)(Math.random() * ((maxValue-0)+1));
     }
   }
   return arr;
 }
   public static void main(String[]args){
+    int argument;
+    if(args.length == 0){
+      argument = 5;
+    }
+    else
+    argument = Integer.parseInt(args[0]);
     int[]x = {1};
     int[]y = {1,2};
     int[]w = {1,2,3};
@@ -49,6 +66,7 @@ public static int[][] create2DArray(int rows, int cols, int maxValue){
     System.out.println(arrayDeepToString(b));
     System.out.println(arrayDeepToString(a).replace("},","},\n"));
     System.out.println(arrayDeepToString(b).replace("},","},\n"));
-    System.out.println(arrayDeepToString(create2DArray(5,3,5)).replace("},","},\n"));
+    System.out.println(arrayDeepToString(create2DArray(argument,3,5)).replace("},","},\n"));
+    System.out.println(arrayDeepToString(create2DArrayRandomized(10,10,10)).replace("},","},\n"));
   }
 }
