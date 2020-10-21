b819c41e321a6cd83d68a1d9d5db1fe0ff6eedc8
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-10-21 14:18:52 -0400

    I tested this program and it works now

--- Demo.java
+++ Demo.java
@@ -7,6 +7,27 @@ public class Demo{
     System.out.println();
   }
 }
+public static String arrToString(int[] arr){
+String result = "{";
+if(arr.length == 0){
+    return "{}";
+  }
+for(int i = 0; i < arr.length; i++){
+  if(i == arr.length-1){
+    result = result + arr[i] + "}";
+  }
+  else
+  result = result + arr[i] + ", ";
+}
+return result;
+}
+public static String arrDeepToString(int[][] arr){
+String result = "";
+String nums1 = arrToString(arr[0]);
+String nums2 = arrToString(arr[1]);
+result = "{" + nums1 + ", " + nums2 + "}";
+return result;
+}
   public static void main(String[]args){
     printLoop(5);
     printLoop(10);
